2f3ff68a3a57b99695e549bafa7b1bb6e06f57da
emilyjf <flanagan.emily.j@gmail.com>
2017-05-08 21:32:01 -0500

    Monday

--- splitting_arrays.rb
+++ splitting_arrays.rb
@@ -1,11 +1,23 @@
 # Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
 
 def split_array(array, array_size)
-  split_array = []
-  x = array_size
-  split_array << array.each_slice(x).to_a
-  puts split_array
+  counter = 0
+  smaller_array = []
+  bigger_array = []
+  array.each do |element|
+    if counter % array_size == array_size - 1
+      smaller_array << element
+      bigger_array << smaller_array
+      smaller_array = []
+    else
+      smaller_array << element
+    end
+    counter += 1
+  end
+  bigger_array
 end
+
+
 
 # Driver code - don't touch anything below this line.
 puts "TESTING split_array..."
