14cb3f7a9486556dc38c6be7507b964d0fa1917b
emilyjf <flanagan.emily.j@gmail.com>
2017-05-22 15:13:34 -0500

    Monday

--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -3,15 +3,25 @@
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
-  array = range.split(" ")
-  alphabet = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26}
-  alphabet.keys each do |letter|
-    letter == array
-      if true 
-  
-  alphabet.each do |key, value|
-    
+  results = []
+  range.chars.each_slice(2) do |a, b|
+    next if b.nil?
+    results << a.next unless a.next == b
   end
+
+  return nil if results.empty?
+  results
+end
+
+  # array = range.split(" ")
+  # alphabet = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26}
+  # alphabet.keys each do |letter|
+  #   letter == array
+  #     if true 
+  
+  # alphabet.each do |key, value|
+    
+  # end
 
 # Driver code - don't touch anything below this line.
 puts "TESTING find_missing_letter..."
